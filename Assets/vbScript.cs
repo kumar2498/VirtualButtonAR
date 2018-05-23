6ac740478e663afd9367e9130048157434afb156
240S-official <240S-official@users.noreply.github.com>
2018-05-24 00:33:50 +0530

    Initial commit

---
+++ Assets/vbScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject vbButtonObject;
+
+	// Use this for initialization
+	void Start () {
+
+        vbButtonObject = GameObject.Find("actionButton");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+
+        Debug.Log("Button Pressed !!!!");
+
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+
+    }
+}
